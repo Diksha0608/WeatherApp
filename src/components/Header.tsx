d7257940fa5cca46eeb776e0a693b7974c49b023
Diksha <dikshadishu0608@gmail.com>
2025-07-23 09:20:51 +0200

    working on styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-   <header>
-    <div>
+   <header className='sticky top-0 z-50 w-full border-b
+    bg-background/95 backdrop-blur py-2 
+    supports-[backdrop-filter]:bg-background/60'>
+    <div  className='container mx-auto flex h-16 items-center justify-between px-4'>
         <Link to={'/'}>
         <img src="/logo.png" alt="climete logo" className='h-14' />
         </Link>
+        <div>
+            {/* theme toggle */}
+        </div>
+
     </div>
    </header>
   )
