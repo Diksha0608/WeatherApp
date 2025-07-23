120cf22167f92ff7a292cfdbf81df9357904ebf6
Diksha <dikshadishu0608@gmail.com>
2025-07-23 15:48:09 +0200

    work in progress

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,6 +1,8 @@
-import { API_CONFIG } from "./config"
 
-class weatherAPI {
+import { API_CONFIG } from "./config"
+import type { Coordinates, ForecastData, GeocodingResponse, WeatherData } from "./types"
+
+class WeatherAPI {
     private createUrl(
         endpoint: string,
         params: Record<string, string | number>
@@ -12,9 +14,46 @@ class weatherAPI {
         return `${endpoint}?${searchParams.toString}`
     }
 
-    private fetchData() { }
+      private async fetchData<T>(url:string): Promise<T> {
+const response = await fetch(url)
 
-    async getCurrentWeather() { }
-    async getForecast() { }
-    async reverseGeoCode() { }
+if(!response.ok){
+    throw new Error(`weather API Error:${response.statusText} `)
 }
+
+return response.json();
+
+       }
+
+    async getCurrentWeather({lat,lon}:Coordinates):Promise<WeatherData> { 
+        const url = this.createUrl(`${API_CONFIG.API_KEY}/weather`,
+            {
+                lat:lat.toString(),
+                lon:lon.toString(),
+                units:API_CONFIG.DEFAULT_PARAMS.units
+            });
+            return this.fetchData<WeatherData>(url)
+    }
+      async getForecast({lat,lon}:Coordinates):Promise<ForecastData> { 
+        const url = this.createUrl(`${API_CONFIG.API_KEY}/forecast`,
+            {
+                lat:lat.toString(),
+                lon:lon.toString(),
+                units:API_CONFIG.DEFAULT_PARAMS.units
+            });
+            return this.fetchData<ForecastData>(url)
+    }
+
+        async reverseGeoCode({lat,lon}:Coordinates):Promise<GeocodingResponse[]> { 
+        const url = this.createUrl(`${API_CONFIG.GEO}/reverse`,
+            {
+                lat:lat.toString(),
+                lon:lon.toString(),
+                limit:1,
+            });
+            return this.fetchData<GeocodingResponse[]>(url)
+    }
+
+}
+
+export const weatherAPI = new WeatherAPI();
--- src/pages/weather_dashboard.tsx
+++ src/pages/weather_dashboard.tsx
@@ -1,9 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
 
 
 const WeatherDashboard = () => {
   return (
-    <div>
-      Weather
+    <div className="space-y-4">
+      {/* Favotite cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button variant={'outline'}
+        size={'icon'}
+        // onClick={handleRefresh}
+
+        >
+          <RefreshCcw className="h-4 w-4"/>
+        </Button>
+      </div>
+
+      {/* current and hourly weather */}
     </div>
   )
 }
