e8620de37eb9170eccf077ca5ff858eecc573264
Diksha <dikshadishu0608@gmail.com>
2025-07-23 11:38:58 +0200

    created components

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,18 @@ import { ThemeProvider } from "./context/theme-provider"
 import Layout from "./components/Layout"
 import WeatherDashboard from "./pages/weather_dashboard"
 import CityPage from "./pages/city_page"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
+     <QueryClientProvider client={queryClient}>
+      
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark">
         <Layout>
@@ -19,8 +28,9 @@ function App() {
           </Routes>
         </Layout>
       </ThemeProvider>
-
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
---
+++ src/api/weather.ts
@@ -0,0 +1,20 @@
+import { API_CONFIG } from "./config"
+
+class weatherAPI {
+    private createUrl(
+        endpoint: string,
+        params: Record<string, string | number>
+    ) {
+        const searchParams = new URLSearchParams({
+            appid: API_CONFIG.API_KEY,
+            ...params,
+        })
+        return `${endpoint}?${searchParams.toString}`
+    }
+
+    private fetchData() { }
+
+    async getCurrentWeather() { }
+    async getForecast() { }
+    async reverseGeoCode() { }
+}
