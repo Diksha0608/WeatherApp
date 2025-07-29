65ff112de5fa542a00a29962d9663899068d392a
Diksha <dikshadishu0608@gmail.com>
2025-07-29 12:54:35 +0200

    creating custom hooks

---
+++ src/hooks/use-geolocation.tsx
@@ -0,0 +1,30 @@
+import type { Coordinates } from "@/api/types";
+import { useEffect, useState } from "react";
+
+interface GeolocationState{
+     coordinates:Coordinates | null;
+     error: string | null;
+     isLoading:boolean;
+
+}
+
+export function useGeolocation{
+   const [locationData, useLocationData] = useState<GeolocationState>({
+    coordinates:null,
+    error:null,
+    isLoading:true
+   });
+
+   const getLocation = () =>{
+
+   }
+
+   useEffect(()=>{
+    getLocation()
+   },[]);
+
+   return {
+    ...locationData,
+    getLocation
+   };
+}
