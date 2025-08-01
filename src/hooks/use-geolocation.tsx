87b5f658e3766f4e058cb19e05735b206f4725b9
Diksha <dikshadishu0608@gmail.com>
2025-08-01 16:31:14 +0200

    completed custom hooks

--- src/hooks/use-geolocation.tsx
+++ src/hooks/use-geolocation.tsx
@@ -1,30 +1,83 @@
 import type { Coordinates } from "@/api/types";
 import { useEffect, useState } from "react";
 
-interface GeolocationState{
-     coordinates:Coordinates | null;
-     error: string | null;
-     isLoading:boolean;
+interface GeolocationState {
+   coordinates: Coordinates | null;
+   error: string | null;
+   isLoading: boolean;
 
 }
 
-export function useGeolocation{
-   const [locationData, useLocationData] = useState<GeolocationState>({
-    coordinates:null,
-    error:null,
-    isLoading:true
+export function useGeolocation() {
+   const [locationData, setLocationData] = useState<GeolocationState>({
+      coordinates: null,
+      error: null,
+      isLoading: true
    });
 
-   const getLocation = () =>{
+   const getLocation = () => {
+      setLocationData((prev) => ({
+         ...prev, isLoading: true, error: null
+      }));
+      if (!navigator.geolocation) {
+         setLocationData({
+            coordinates: null,
+            error: "Geolocation is not supported by your browser",
+            isLoading: false,
+         })
+         return;
+      };
+
+      navigator.geolocation.getCurrentPosition((position) => {
+         setLocationData({
+            coordinates: {
+               lat: position.coords.latitude,
+               lon: position.coords.longitude
+            },
+            error: null,
+            isLoading: false,
+         })
+      }, (error) => {
+         let errormessage: string
+         switch (error.code) {
+            case error.PERMISSION_DENIED:
+               errormessage = "Location permission denied. Please enable location access";
+               break;
+            case error.POSITION_UNAVAILABLE:
+               errormessage = "Location information is unavailabel";
+               break;
+
+            case error.TIMEOUT:
+               errormessage = "Location Request time out";
+               break;
+            default:
+               errormessage="Am unknown error occured."
+}
+
+setLocationData(
+   {
+            coordinates: null,
+            error: errormessage,
+            isLoading: false,
+         }
+)
+
+      },{
+enableHighAccuracy:true,
+timeout: 5000,
+maximumAge:0
+      }
+     
+   )
 
    }
 
-   useEffect(()=>{
-    getLocation()
-   },[]);
+   useEffect(() => {
+      getLocation()
+   }, []);
 
    return {
-    ...locationData,
-    getLocation
+      ...locationData,
+      getLocation
    };
 }
