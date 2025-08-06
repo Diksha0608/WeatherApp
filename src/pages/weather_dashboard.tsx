519082a6cec35cbc630e7ed1321d01fa52345f2f
Diksha <dikshadishu0608@gmail.com>
2025-08-06 19:25:56 +0200

    work in progress

--- src/pages/weather_dashboard.tsx
+++ src/pages/weather_dashboard.tsx
@@ -1,8 +1,32 @@
+import WeatherSkeleton from "@/components/loading-skeleton";
 import { Button } from "@/components/ui/button";
+import { useGeolocation } from "@/hooks/use-geolocation";
 import { RefreshCcw } from "lucide-react";
 
 
 const WeatherDashboard = () => {
+   const {
+    coordinates,
+    error:locationError,
+    getLocation,
+    isLoading:locationLoading
+   } 
+   =useGeolocation();
+
+   console.log(coordinates)
+
+   const handleRefresh = ()=>{
+    getLocation();
+    if(coordinates){
+      // reload weather data
+    }
+   };
+
+   if(locationLoading){
+return <WeatherSkeleton/>
+   }
+   
+
   return (
     <div className="space-y-4">
       {/* Favotite cities */}
@@ -10,7 +34,7 @@ const WeatherDashboard = () => {
         <h1 className="text-xl font-bold tracking-tight">My Location</h1>
         <Button variant={'outline'}
         size={'icon'}
-        // onClick={handleRefresh}
+        onClick={handleRefresh}
 
         >
           <RefreshCcw className="h-4 w-4"/>
